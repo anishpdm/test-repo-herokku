ee5a8d1347f4b32960f6f725f2d1c0e36f682eda
anishpdm <anish.vilayil.s@gmail.com>
2018-11-13 22:05:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+@app.route('/send',methods=['GET','POST'])
+def send():
+    if(request.method=='POST'):
+        getName=request.form['name']
+        getEmail=request.form['email']
+        return render_template('result.html',newName=getName,newEmail=getEmail)
+
+@app.route('/contact')
+def con():
+    return render_template('contactus.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# @app.route('/contact')
+# def contact():
+#     return render_template('contactus.html')
+
+
+if(__name__=='__main__'):
+    app.run(debug=True)
